a79ddf430c31291f6503d4827c1852419792cc32
alokkumax <alokkumax1@gmail.com>
2020-10-29 00:49:53 +0530

    Animated-Search_Bar

---
+++ lib/homePage.dart
@@ -0,0 +1,25 @@
+import 'package:animated_search_bar/searchBar.dart';
+import 'package:flutter/material.dart';
+
+
+
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //
+        // title: PabelLogo(),
+      ),
+      backgroundColor: Colors.white,
+
+      floatingActionButton: searchBar(),
+    );
+  }
+}
